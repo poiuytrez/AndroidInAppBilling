6cf5ff42a675c79075f12cf1149d19b332d3b7af
poiuytrez <guillaumecharhon@gmail.com>
2013-05-23 14:01:30 +0200

    version 3

---
+++ v3/com/inappbilling.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2012-2013 by Guillaume Charhon
+ */
+var inappbilling = { 
+
+	// Initialize the plugin
+    init: function (success, fail) { 
+      return cordova.exec( success, fail, 
+                           "InAppBillingPlugin", 
+                           "init", ["null"]); 
+    },
+    // get already own items
+    getPurchases: function (success, fail) {
+        return cordova.exec( success, fail,
+            "InAppBillingPlugin",
+            "getPurchases", ["null"]);
+    },
+    // purchase an item
+    buy: function (success, fail, productId) {
+        return cordova.exec( success, fail,
+            "InAppBillingPlugin",
+            "buy", [productId]);
+    },
+    // purchase an item
+    subscribe: function (success, fail, productId) {
+        return cordova.exec( success, fail,
+            "InAppBillingPlugin",
+            "subscribe", [productId]);
+    },
+    // consume a purchased item
+    consumePurchase: function (success, fail, productId) {
+        return cordova.exec( success, fail,
+            "InAppBillingPlugin",
+            "consumePurchase", [productId]);
+    }
+};
