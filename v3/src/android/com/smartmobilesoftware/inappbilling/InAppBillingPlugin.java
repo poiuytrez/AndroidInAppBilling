2250e7b7870343f716d4608ad1344532ddcac0da
Sebastien Roch <roch.sebastien@gmail.com>
2014-05-19 21:46:27 +0200

    The callback should receive a json object, not a json string

--- v3/src/android/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
+++ v3/src/android/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
@@ -369,7 +369,7 @@ public class InAppBillingPlugin extends CordovaPlugin {
             
             // add the purchase to the inventory
             myInventory.addPurchase(purchase);
-            callbackContext.success(purchase.getOriginalJson());
+            callbackContext.success(new JSONObject(purchase.getOriginalJson()));
 
         }
     };
